ef685d370bb8bb3a16edf3102d6300677dce4d39
Ditte Aarup Kamstrup <ditt4030@edu.easj.dk>
2016-11-13 23:34:26 +0100

    Er nu på develop branch - tekst added

--- BranchOpgave/Program.cs
+++ BranchOpgave/Program.cs
@@ -11,6 +11,8 @@ namespace BranchOpgave
         static void Main(string[] args)
         {
             Console.WriteLine("Test af Git Branch");
+
+            Console.WriteLine("Er nu på develop branch");
         }
     }
 }
